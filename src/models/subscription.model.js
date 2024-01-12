722b43129133b8442b9af29c2339697fc5f62e1e
codeCampWithSifat <sayedhossainsifat300@.com>
2024-01-13 01:25:59 +0600

    like model comment model playlist model tweet model part are done

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,14 +1,13 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-const subscriptionSchema = new mongoose.Schema(
+const subscriptionSchema = new Schema(
   {
     subscriber: {
-      type: mongoose.Schema.Types.ObjectId, // one who is subscribing
+      type: Schema.Types.ObjectId, // one who is subscribing
       ref: "User",
     },
-
     channel: {
-      type: mongoose.Schema.Types.ObjectId, // one to whom is subscriber
+      type: Schema.Types.ObjectId, // one to whom 'subscriber' is subscribing
       ref: "User",
     },
   },
